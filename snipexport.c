0d049b92dee6cf409d5b7450d348b0d90d2d31ac
Bruno BEAUFILS <bruno@boulgour.com>
2009-12-15 07:40:39 +0100

    Fixed typos and added Copyright statement.

--- snipexport.c
+++ snipexport.c
@@ -1,7 +1,13 @@
 /*
  * Export snipsnap data dump into files
+ *
+ * Copyright (C) 2009 Bruno BEAUFILS <bruno@boulgour.com>
  * 
- * Bruno BEAUFILS <bruno@boulgour.com>
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
  */
 
 #include <stdio.h>
